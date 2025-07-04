9f22bc58c172ca122a1dceaf05af473b23621eff
Johan <jmora@promarketingchile.com>
2025-07-04 15:15:37 -0400

    feat: Add manual trigger workflow and notification script

---
+++ triggers-automatic.js
@@ -0,0 +1,3 @@
+const developer = process.env.DEVELOPER;
+const requester = process.env.REQUESTER;
+console.log(`Al frente y al centro ${developer} se disparo un trigger automatico, solicitado por ${requester}`);
